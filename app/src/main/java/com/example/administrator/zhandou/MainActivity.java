4403d27bfb45c147b6c0eb1115c672ddd98293f2
Administrator <310099657@qq.com>
2019-03-11 09:33:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/zhandou/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.administrator.zhandou;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Hello World!
+     */
+    private TextView mTtvvvv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        //mTtvvvv = (TextView) findViewById(R.id.ttvvvv);
+    }
+}
